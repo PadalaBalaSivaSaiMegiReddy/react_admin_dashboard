92ff09f225d8c3c08934bd7de2c66d98b2759a23
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-29 11:39:00 +0530

    Refactor sidebar styling and add icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,34 +1,86 @@
-import './Sidebar.css'
-import LineStyleIcon from '@mui/icons-material/LineStyle';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import TimelineIcon from '@mui/icons-material/Timeline';
+import "./Sidebar.css";
+import LineStyleIcon from "@mui/icons-material/LineStyle";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TimelineIcon from "@mui/icons-material/Timeline";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
-          <h3 className="sidebarTitle">
-            Dashboard
-          </h3>
+          <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-            <LineStyleIcon/>
-            Home
+            <li className="sidebarListItem active">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
             </li>
             <li className="sidebarListItem">
-            <TimelineIcon/>
-            Analytics
+              <TimelineIcon  className="sidebarIcon" />
+              Analytics
             </li>
             <li className="sidebarListItem">
-            <TrendingUpIcon/>
-            Sales 
+              <TrendingUpIcon className="sidebarIcon"  />
+              Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon  className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className="sidebarIcon"  />
+              Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon  className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className="sidebarIcon"  />
+              Sales
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon  className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className="sidebarIcon"  />
+              Sales
             </li>
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
