27dc29269526d3f410c1f2bbbc9681ce7e9f89f3
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-30 22:06:40 +0530

    Update favicon link and add new widgets to home page

---
+++ src/components/widgetLg/WidgetLg.jsx
@@ -0,0 +1,9 @@
+import './WidgetLg.css'
+
+function WidgetLg() {
+  return (
+    <div className='widgetLg'>WidgetLg</div>
+  )
+}
+
+export default WidgetLg
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,18 @@ import './Home.css';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart';
 import { userData } from '../../dummyData';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 
 function Home() {
   return (
     <div className='home'>
       <FeaturedInfo/>
       <Chart data={userData} title="Active User Details" grid dataKey="Active User"/>
-      <div className="homeWidgets"></div>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
       
     </div>
   )
