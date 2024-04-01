a534ebeef2b713719f97fc197c53000c9629ed03
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-01 13:55:27 +0530

    Add @mui/x-data-grid and UserList component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Sidebar from './components/sidebar/Sidebar'
 import Topbar from './components/topbar/Topbar'
 import Home from './pages/home/Home'
+import UserList from './pages/userList/UserList'
 
 function App() {
 
@@ -15,7 +16,9 @@ function App() {
         <Sidebar/>
         <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path="/users" element={<UserList/>}/>
         </Routes>
+
         </div>
       </div>
     </>
