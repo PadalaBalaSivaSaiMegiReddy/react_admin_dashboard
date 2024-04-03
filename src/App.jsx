fbb5acd56c0ec90e5e0f2907065f7cd9bb107587
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-03 12:29:11 +0530

    Add product-related pages and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Home from './pages/home/Home'
 import UserList from './pages/userList/UserList'
 import User from './pages/user/User'
 import NewUser from './pages/newuser/NewUser'
+import ProductList from './pages/productlist/ProductList'
+import Product from './pages/product/Product'
+import NewProduct from './pages/newproduct/NewProduct'
 
 function App() {
 
@@ -21,6 +24,9 @@ function App() {
         <Route path="/users" element={<UserList/>}/>
         <Route path="/user/:userId" element={<User/>}/>
         <Route path="/newUser" element={<NewUser/>}/>
+        <Route path="/products" element={<ProductList/>}/>
+        <Route path="/product/:productId" element={<Product/>}/>
+        <Route path="/newProduct" element={<NewProduct/>}/>
 
 
         </Routes>
